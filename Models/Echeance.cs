972404e4874494f95949fad769599073d101ba68
Fonsa Dabo <kaoudabo1@gmail.com>
2025-10-18 12:39:51 +0200

    Ajoutez des fichiers projet.

---
+++ Models/Echeance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationsPretsBancaires.Models
+{
+    internal class Echeance
+    {
+        public int Id { get; set; }
+        public int PretId { get; set; }
+        public int Numero { get; set; }        // numéro de la mensualité
+        public DateTime Date { get; set; }
+        public double Interets { get; set; }
+        public double MontantPrincipal { get; set; }
+        public double ResteDu { get; set; }
+    }
+}
---
+++ Models/Prets.cs
@@ -0,0 +1,21 @@
+﻿using SimulationsPretsBancaires.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SimulationsPretsBancaires.Models
+{
+    internal class Prets
+    {
+        public int Id { get; set; }
+        public string Emprunteur { get; set; }
+        public double Capital { get; set; }           // montant du prêt
+        public double TauxAnnuel { get; set; }       // en pourcentage, ex: 4.5
+        public int DureeMois { get; set; }           // durée en mois
+        public DateTime DateDebut { get; set; } 
+        
+    }
+}
